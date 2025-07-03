b13ea55b7ab413ab458fdd3caaa67d99f2feaa7c
pmburden@gmail.com <pmburden@gmail.com>
2025-07-03 03:51:54 +0000

    add test load yaml

--- macros/utils/test_load_yaml.sql
+++ macros/utils/test_load_yaml.sql
@@ -1,5 +1,5 @@
 {% macro test_load_yaml() %}
-  {% set data = load_yaml('models/generated/generated_circuits.yml.yml') %}
+  {% set data = load_yaml('models/core/dim_circuits.yml') %}
   {{ log(data, info=True) }}
   {{ return(data) }}
 {% endmacro %}
